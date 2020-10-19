b1bfce0cc3703cd9935d3fab4b6d8fdd1613fd1c
Binoy Joseph <binoy.joseph@decipherworks.com>
2020-10-19 12:29:37 +1100

    added okta provider version

--- password_policy.tf
+++ password_policy.tf
@@ -21,6 +21,5 @@ resource "okta_policy_password" "example" {
   status                 = "INACTIVE"
   description            = "Example"
   password_history_count = 4
-  groups_included        = ["${data.okta_group.everyone.id}"]
   priority               = 5
 }
